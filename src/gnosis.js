36521ef578838508dabfa091c35bb0ac14662389
ifandelse <jim@ifandelse.com>
2014-02-28 16:30:29 -0500

    Prepping for v0.1.0

--- src/gnosis.js
+++ src/gnosis.js
@@ -14,7 +14,8 @@
         root.gnosis = factory(root);
     }
 }(this, function(global, undefined) {
-    var getKeys = function(target, options) {
+    var gExclude = ["_gTree", "rescan"];
+    var getKeys = function getKeys(target, options) {
         var keys = [];
         var kind = getType(target);
         if (options.ownProps) {
@@ -32,19 +33,23 @@
         return keys;
     };
 
-    var getType = function(target) {
+    var getType = function getType(target) {
         var kind = Object.prototype.toString.call(target);
         return kind.substring(8, kind.length - 1).toLowerCase();
     };
 
     var nodeTypes = {
-        "traversable": function(target, key, kind, transformFn, options, path, root) {
+        "traversable": function traversable(target, key, kind, transformFn, options, path, root) {
             var val = target[key];
-            transformFn(target, key, val, kind, path, root);
+            if (!options.rescan || (options.rescan && !root._gTree[path])) {
+                transformFn(target, key, val, kind, path, root);
+            }
             traverse(val, transformFn, options, path, root);
         },
-        "simple": function(target, key, kind, transformFn, options, path, root) {
-            transformFn(target, key, target[key], kind, path, root);
+        "simple": function simple(target, key, kind, transformFn, options, path, root) {
+            if (!options.rescan || (options.rescan && !root._gTree[path])) {
+                transformFn(target, key, target[key], kind, path, root);
+            }
         }
     };
 
@@ -54,21 +59,38 @@
             options = {};
         }
         options = options || {};
-        path = path || ["{ROOT}"];
+        path = path || "{ROOT}";
         options.arrayMembers = options.arrayMembers || traverse.arrayMembers;
         options.objectMembers = options.objectMembers || traverse.objectMembers;
         root = root || target;
+        if (!root._gTree) {
+            root._gTree = {};
+            root._gTree[path] = getType(target);
+        }
+        if (!root.rescan) {
+            root.rescan = function() {
+                traverse(target, transformFn, $.extend({}, options, {
+                    rescan: true
+                }), path, root);
+            }
+        }
         var keys = getKeys(target, options);
         var idx = 0;
         var member;
         var kind;
         var key;
+        var nextPath;
         while (idx < keys.length) {
             key = keys[idx];
+            idx += 1;
+            if (gExclude.indexOf(key) !== -1) {
+                continue;
+            }
             member = target[key];
             kind = getType(member);
-            nodeTypes[kind !== "array" && kind !== "object" ? "simple" : "traversable"](target, key, kind, transformFn, options, path.concat([key]), root);
-            idx += 1;
+            nextPath = path + "." + key;
+            nodeTypes[kind !== "array" && kind !== "object" ? "simple" : "traversable"](target, key, kind, transformFn, options, nextPath, root);
+            root._gTree[nextPath] = kind;
         }
     };
     // defaults for array and object prototype members to include in traversal
@@ -77,7 +99,7 @@
     // for *any* traversals that happen
     traverse.arrayMembers = ["pop", "push", "reverse", "shift", "sort", "splice", "unshift"];
     traverse.objectMembers = [];
-    traverse.getType = getType;
+    traverse.exclude = gExclude;
     return {
         traverse: traverse,
         getType: getType
